5af652ee31e173426a06495af55a41fc333dff83
PTPhongKMF <ptphongkmf@luxcadia.com>
2026-07-05 00:54:41 +0700

    style(app/layouts): add responsive `svi` text size to `RootLayout`

--- src/app/layouts/root-layout.tsx
+++ src/app/layouts/root-layout.tsx
@@ -4,9 +4,9 @@ import Icon from "#shared/ui/icon/icon.tsx";
 
 export default function RootLayout() {
   return (
-    <div class="grid w-full auto-rows-auto grid-cols-1">
+    <div class="grid w-full auto-rows-auto grid-cols-1 text-vp-base">
       <div class="grid min-h-dvh grid-rows-[auto_1fr]">
-        <header class="@container bg-yellow-200 px-[clamp(0.25rem,4dvi,3rem)] py-1 text-2xl text-walnut">
+        <header class="@container bg-yellow-200 px-[clamp(0.25rem,4dvi,3rem)] py-1 text-vp-2xl text-walnut">
           <Link
             to="/"
             class="inline-block font-pencil text-fluid-4xl font-bold transition-all duration-200 hover:scale-105 hover:rotate-3 hover:text-amber-800"
@@ -20,7 +20,7 @@ export default function RootLayout() {
         </main>
       </div>
 
-      <footer class="@container grid grid-cols-2 items-center bg-amber-200 px-[clamp(0.25rem,2dvi,3rem)] py-1 text-fluid-xs">
+      <footer class="@container grid grid-cols-2 items-center bg-amber-200 px-[clamp(0.25rem,2dvi,3rem)] py-1 text-vp-xs">
         <p class="justify-self-start">Copyright © 2026 PTPhongKMF</p>
 
         <section class="flex flex-wrap items-center justify-end gap-x-4 gap-y-2 justify-self-end">
