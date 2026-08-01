241dd34f12d38084fb8e5af5ab6a3ff63ffc139e
PTPhongKMF <ptphongkmf@luxcadia.com>
2026-08-01 11:25:17 +0700

    fix(app/router): give correct `queryOptions` to `prefetch` and `fetch` in route loader

--- src/app/router/routes/notes.$id.tsx
+++ src/app/router/routes/notes.$id.tsx
@@ -2,20 +2,17 @@ import { createFileRoute } from "@tanstack/solid-router";
 import Home from "#pages/home/home.tsx";
 import { queryClient } from "#shared/api/query-client.ts";
 import {
-  noteQueryDetailOrDefaultKey,
+  noteQueryDetailOrDefault,
 } from "#entities/note/api/queries/note-detail-or-default-query.ts";
 import { setLastOpenedNoteId } from "#entities/note/api/last-opened-note.ts";
-import {
-  noteQueryListKey,
-} from "#entities/note/api/queries/note-list-query.ts";
+import { noteQueryList } from "#entities/note/api/queries/note-list-query.ts";
 
 export const Route = createFileRoute("/notes/$id")({
   loader: async ({ params }) => {
-    queryClient.prefetchQuery({ queryKey: noteQueryListKey.all() });
+    queryClient.prefetchQuery(noteQueryList());
 
-    await queryClient.fetchQuery({
-      queryKey: noteQueryDetailOrDefaultKey.detailOrDefault(params.id),
-    }).catch(/* ignore */);
+    await queryClient.fetchQuery(noteQueryDetailOrDefault(params.id))
+      .catch(/* ignore */);
 
     setLastOpenedNoteId(params.id);
   },
