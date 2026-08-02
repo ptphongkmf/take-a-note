aefcec4fe00c9ba624ff3ebd688194ddd36b3b05
PTPhongKMF <ptphongkmf@luxcadia.com>
2026-08-02 17:31:01 +0700

    feat(pages/home): make note action buttons more snappy

--- src/pages/home/ui/note-action.tsx
+++ src/pages/home/ui/note-action.tsx
@@ -2,7 +2,7 @@ import type { ComponentProps } from "solid-js";
 import { splitProps } from "solid-js";
 import { c } from "#shared/lib/class-merger/c.ts";
 import Icon from "#shared/ui/icon/icon.tsx";
-import Button from "#shared/ui/button/button.tsx";
+import SnappyButton from "#shared/ui/button/snappy-button.tsx";
 
 export default function NoteAction(props: ComponentProps<"div">) {
   const [local, others] = splitProps(props, ["class"]);
@@ -13,43 +13,43 @@ export default function NoteAction(props: ComponentProps<"div">) {
       {...others}
       class={c("@container grid auto-rows-auto grid-cols-2 gap-4", local.class)}
     >
-      <Button
+      <SnappyButton
         type="button"
         class="col-span-2 flex items-center justify-center gap-2 hover:bg-amber-100"
       >
         <Icon name="file-plus" class="text-amber-800" />
         <span>New note</span>
-      </Button>
-      <Button
+      </SnappyButton>
+      <SnappyButton
         type="button"
         class="col-span-2 flex items-center justify-center gap-2 hover:bg-amber-100"
       >
         <Icon name="file-x" class="text-amber-800" />
         <span>Delete note</span>
-      </Button>
+      </SnappyButton>
 
-      <Button
+      <SnappyButton
         type="button"
         class="flex items-center justify-center gap-2 hover:bg-amber-100"
       >
         <Icon name="download" class="text-amber-800" />
         <span>Import</span>
-      </Button>
-      <Button
+      </SnappyButton>
+      <SnappyButton
         type="button"
         class="flex items-center justify-center gap-2 hover:bg-amber-100"
       >
         <Icon name="upload" class="text-amber-800" />
         <span>Export</span>
-      </Button>
+      </SnappyButton>
 
-      <Button
+      <SnappyButton
         type="button"
         class="col-span-2 flex items-center justify-center gap-2 hover:bg-amber-100"
       >
         <Icon name="share-2" class="text-amber-800" />
         <span>Share</span>
-      </Button>
+      </SnappyButton>
     </div>
   );
 }
--- src/shared/ui/button/snappy-button.tsx
+++ src/shared/ui/button/snappy-button.tsx
@@ -2,7 +2,7 @@ import type { ComponentProps } from "solid-js";
 import { splitProps } from "solid-js";
 import { c } from "#shared/lib/class-merger/c.ts";
 
-export default function Button(props: ComponentProps<"button">) {
+export default function SnappyButton(props: ComponentProps<"button">) {
   const [local, others] = splitProps(props, ["class", "children"]);
 
   return (
@@ -11,7 +11,7 @@ export default function Button(props: ComponentProps<"button">) {
       {...others}
       class={c(
         "size-fit w-full cursor-pointer rounded-md px-3 py-1.5",
-        "transition-colors duration-150 hover:bg-neutral-200/60",
+        "transition-colors duration-0 hover:bg-neutral-200/60 hover:duration-75 hover:ease-out",
         "disabled:pointer-events-none disabled:opacity-50",
         "focus-visible:ring-2 focus-visible:ring-ring focus-visible:outline-none",
         local.class,
